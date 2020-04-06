577fbeb78aa01f6bf369abce26cf39353090f4c8
Xinsong Li <lixinso@gmail.com>
2020-04-06 22:15:01 +0000

    Create psutil_virtual_memory.py

---
+++ psutil_virtual_memory.py
@@ -0,0 +1,3 @@
+import psutil
+print(psutil.virtual_memory())
+#svmem(total=4093456384, available=2330390528, percent=43.1, used=1431490560, free=191127552, active=1729134592, inactive=2001334272, buffers=95653888, cached=2375184384, shared=84795392, slab=97583104)
