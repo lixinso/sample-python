8289ca1cd4e78307827f872d32fa7e6fce716919
Xinsong Li <lixinso@gmail.com>
2020-12-18 00:23:03 -0600

    list copy

--- list_copy.py
+++ list_copy.py
@@ -1,1 +1,12 @@
+>>> arr = ['a','b','c']
+>>> arr_copy = arr.copy()
+>>> arr
+['a', 'b', 'c']
+>>> arr_copy
+['a', 'b', 'c']
+>>> del arr_copy[1]
+>>> arr
+['a', 'b', 'c']
+>>> arr_copy
+['a', 'c']
 
