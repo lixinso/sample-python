cbe874b37bee46529160e113eec9814f4b36168d
Xinsong Li <lixinso@gmail.com>
2020-12-19 13:50:44 -0600

    priority queue

---
+++ priority_queue.py
@@ -0,0 +1,18 @@
+from queue import PriorityQueue
+
+q = PriorityQueue()
+
+input = ['a','c','b']
+
+print("Input:", input)
+
+for x in input:
+    q.put(x)
+
+
+print("From Priority Queue:")
+while not q.empty():
+    print(q.get())
+
+# https://www.geeksforgeeks.org/priority-queue-in-python/
+# https://docs.python.org/3/library/queue.html
