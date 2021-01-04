8de56e5808cde9094fee3bfae6f500abaef82a78
Xinsong Li <lixinso@gmail.com>
2021-01-04 15:34:42 -0600

    Update and rename list_insert.py to list_insert_extend.py

---
+++ list_insert_extend.py
@@ -0,0 +1,20 @@
+
+# https://www.tutorialspoint.com/python/list_insert.htm
+
+>>> l1=["a","b","c"]
+>>> l1.insert(2,"d")
+>>> l1
+['a', 'b', 'd', 'c']
+>>>
+
+
+>>> my_list = ['ge','fo']
+>>> yo_list = [1,2,3]
+>>> my_list.extend(yo_list)
+>>> my_list
+['ge', 'fo', 1, 2, 3]
+
+
+
+https://www.geeksforgeeks.org/append-extend-python/
+
