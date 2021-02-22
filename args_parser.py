8a672c3f585ed7850e00e7ade44909604978367c
Xinsong Li <lixinso@gmail.com>
2021-02-22 14:04:28 -0600

    args func

--- args_parser.py
+++ args_parser.py
@@ -3,18 +3,32 @@
 import argparse
 
 
+def test_sub(args):
+    print("print from sub")
+
+def test_sub_sub(args):
+    print("print from sub_sub")
+
 def test_sub_sub_sub(args):
     print("print from sub_sub_sub")
 
 parser = argparse.ArgumentParser(prog='PROG')
 parser_subparsers = parser.add_subparsers()
 sub = parser_subparsers.add_parser('sub')
+sub.set_defaults(func=test_sub)
+
+
 sub_subparsers = sub.add_subparsers()
 sub_sub = sub_subparsers.add_parser('sub_sub')
+sub_sub.set_defaults(func=test_sub_sub)
+
 
 sub_sub_subparsers = sub_sub.add_subparsers()
 sub_sub_sub = sub_sub_subparsers.add_parser('sub_sub_sub')
 sub_sub_sub.set_defaults(func=test_sub_sub_sub)
+
+
+
 
 args = parser.parse_args()
 
@@ -28,13 +42,18 @@ if args.func:
 Test:
 
 
+% python3 args_parser.py sub
+print from sub
+
+
+% python3 args_parser.py sub sub_sub    
+print from sub_sub
+
+
 % python3 args_parser.py sub sub_sub sub_sub_sub
 print from sub_sub_sub
 
-% python3 args_parser.py sub sub_sub            
-Traceback (most recent call last):
-  File "args_parser.py", line 22, in <module>
-    if args.func:
-AttributeError: 'Namespace' object has no attribute 'func'
+
+
 
 '''
