85cc931f9cb807222311fdf0af0057703c7bf954
Xinsong Li <lixinso@gmail.com>
2021-02-22 13:49:22 -0600

    args_parser.py

--- args_parser.py
+++ args_parser.py
@@ -1,1 +1,12 @@
 # https://stackoverflow.com/questions/8368110/python-argparse-subcommand-subcommand
+
+
+import argparse
+
+parser = argparse.ArgumentParser(prog='PROG')
+parser_subparsers = parser.add_subparsers()
+sub = parser_subparsers.add_parser('sub')
+sub_subparsers = sub.add_subparsers()
+sub_sub = sub_subparsers.add_parser('sub_sub')
+sub_sub_subparsers = sub_sub.add_subparsers()
+sub_sub_sub = sub_sub_subparsers.add_parser('sub_sub_sub')
